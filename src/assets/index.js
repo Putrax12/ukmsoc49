bb904dcf6ec07b6c47f371aa3dd47402b201334b
Putrax12 <ronjmembe@gmail.com>
2026-05-03 14:25:16 +0700

    compress images to webp

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,14 @@
-import bgImage from "./bg.jpg";
+import bgImage from "./bg.webp";
 import logo from "./logo.png";
-import home from "./home.jpg";
-import away from "./away.jpg";
-import third from "./third.jpg";
-import g1 from "./galeri/1.jpg";
-import g2 from "./galeri/2.jpg";
-import g3 from "./galeri/3.jpg";
-import g4 from "./galeri/4.jpg";
-import g5 from "./galeri/5.jpg";
-import g6 from "./galeri/6.jpg";
+import home from "./home.webp";
+import away from "./away.webp";
+import third from "./third.webp";
+import g1 from "./galeri/1.webp";
+import g2 from "./galeri/2.webp";
+import g3 from "./galeri/3.webp";
+import g4 from "./galeri/4.webp";
+import g5 from "./galeri/5.webp";
+import g6 from "./galeri/6.webp";
 
 export { g1, g2, g3, g4, g5, g6 };
 export { bgImage, logo, home, away, third };
